f3a5dabe116f72941382d2520a8333e2ea28649c
Aidan <ahernaidan1@gmail.com>
2022-12-14 10:55:02 -0500

    initial import

---
+++ engine.h
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+#define width 20
+#define height 20
+
+char world[width][height];
+
+typedef struct player{
+    int health;
+    int maxhealth;
+    int level;
+}player;
+
+typedef struct item{
+    char *name;
+    char icon;
+    int value;
+    double weight;
+
+}item;
+
+typedef struct inventory{
+    item* contents;
+    int size;
+}inventory;
+
+inventory playerInventory;
+
+int turnCount=0;
+
+
+//Definitions---------------------------------------------------------------//
+void turn(){
+    turnCount++;
+}
+
+void drawMap(int playerX, int playerY){
+    system("cls");
+    printf("\033[93m                Valdmir!\n");
+    printf("\033[96mItems: ");
+    //show inventory items
+    for(int i=0; i<playerInventory.size; i++){
+        printf((char*)(playerInventory.contents->icon));
+    }
+    printf("\n");
+
+    // Draw the game world
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            if(world[x][y] == '@')                          // Player
+                printf("\033[33m%c ", world[x][y]);
+            if(world[x][y] == 'w')                          // Wall
+                printf("\033[100m  \033[40m", world[x][y]);
+            if(world[x][y] == '.')                          // Walkable Floor
+                printf("\033[47m  ", world[x][y]);
+        }
+        printf("\n\033[40m");
+    }
+}
+
---
+++ main.c
@@ -0,0 +1,91 @@
+
+#include "engine.h"
+
+#define width 20
+#define height 20
+
+
+int main(void) {
+
+    // Set up the game world
+    int playerX = 3;
+    int playerY = 3;
+
+    //Load Level
+    FILE *fptr;
+    fptr = fopen("1.lvl","r");
+    printf("Loaded Level");
+    if(fptr == NULL) {
+      printf("Error! No level file in directory!");   
+      exit(1);             
+    }
+
+    // Initialize the game world
+    char c;
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            c = fgetc(fptr);
+            if(c == 'w')
+                world[x][y] = 'w';
+            if(c == '0')
+                world[x][y] = '.';
+        }
+    }
+    world[playerX][playerY] = '@';
+    //Sets up Color
+    system("setup.bat");
+
+    drawMap(playerX, playerY);
+
+    // Game loop
+    while (1) {
+        char ch;
+
+        if (kbhit()) {
+            world[playerX][playerY] = '.';
+            ch = getch();
+
+            if(ch == 'w'){     
+                if (playerX-1 >= 0 && playerX-1 < width && playerY >= 0 && playerY < height && world[playerX-1][playerY] != 'w') {
+                    playerX--;
+                    world[playerX][playerY] = '@';
+                    drawMap(playerX, playerY);
+                    turn();
+                }  
+            }
+                
+            if (ch == 'a'){        
+                if (playerX >= 0 && playerX < width && playerY-1 >= 0 && playerY-1 < height && world[playerX][playerY-1] != 'w') {
+                    playerY--;
+                    world[playerX][playerY] = '@';
+                    drawMap(playerX, playerY);
+                }  
+            }
+                
+            if(ch == 's'){
+                if (playerX+1 >= 0 && playerX+1 < width && playerY >= 0 && playerY < height && world[playerX+1][playerY] != 'w'){
+                    playerX++;
+                    world[playerX][playerY] = '@';
+                    drawMap(playerX, playerY);
+                }  
+            }
+            
+            if(ch == 'd'){          
+                if (playerX >= 0 && playerX < width && playerY+1 >= 0 && playerY+1 < height && world[playerX][playerY+1] != 'w'){
+                    playerY++;
+                    world[playerX][playerY] = '@';
+                    drawMap(playerX, playerY);
+            }
+                
+            }               
+
+        }
+
+
+        // Check for end conditions and break out of the game loop if necessary
+    }
+
+    // Clean up and exit
+    system("cleanup.bat");
+    return 0;
+}
